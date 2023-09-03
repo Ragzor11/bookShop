8195f90947c4d4f3f2290b1b621ceaccace6c150
DenisBerezin <compA321@gmail.com>
2023-09-03 15:31:53 +0300

    set up the home page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,7 +2,7 @@ export function Home() {
     
     return (
         <section className="home">
-            <h1>Car's R Us!</h1>
+            <h1>The Best Book Shop!</h1>
             <img src="../assets/img/react.png" alt="" />
         </section>
     )
--- root-cmp.jsx
+++ root-cmp.jsx
@@ -6,23 +6,23 @@ import { Home } from "./pages/Home.jsx";
 
 export function App() {
 
-    const [page, setPage] = useState('car')
+    const [page, setPage] = useState('home')
 
     return (
         <section className="app main-layout">
-            <header className="app-header full main-layout">
-                <h1>React Car App</h1>
+            <header className="app-header full main-layout ">
+                <h1>Book Shop V3</h1>
                 <nav className="app-nav">
                     <a onClick={() => setPage('home')} href="#">Home</a>
                     <a onClick={() => setPage('about')} href="#">About</a>
-                    <a onClick={() => setPage('car')} href="#">Cars</a>
+                    <a onClick={() => setPage('books')} href="#">Books</a>
                 </nav>
             </header>
 
             <main>
                 {page === 'home' && < Home />}
                 {page === 'about' && <About />}
-                {page === 'car' && <CarIndex />}
+                {page === 'books' && <CarIndex />}
             </main>
         </section>
     )
